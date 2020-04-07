ce3e5952a86d6fdd238bae5acbf79c4a09e794de
Σαββας Φιλιππιδης <filippides.savvas@gmail.com>
2020-04-07 17:18:55 +0300

    port from enviroment variable

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const fetch = require("node-fetch");
 const app = express();
 require("dotenv").config();
 
-app.listen(3000, () => console.log("listening from 3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Starting server at ${port}`));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
